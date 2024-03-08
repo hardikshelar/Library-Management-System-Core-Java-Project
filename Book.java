9f5e5519a9c764efb1ea568eb85b3fe0371eaa3c
Hardik Shelar <90783840+hardikshelar@users.noreply.github.com>
2024-03-09 00:21:17 +0530

    Add files via upload

---
+++ Book.java
@@ -0,0 +1,29 @@
+package com.jsp.lms.model;
+
+public class Book {
+	private String bookName;
+	private String bookAuthor;
+	private double bookPrice;
+	public String getBookName() {
+		return bookName;
+	}
+	public void setBookName(String bookName) {
+		this.bookName = bookName;
+	}
+	public String getBookAuthor() {
+		return bookAuthor;
+	}
+	public void setBookAuthor(String bookAuthor) {
+		this.bookAuthor = bookAuthor;
+	}
+	public double getBookPrice() {
+		return bookPrice;
+	}
+	public void setBookPrice(double bookPrice) {
+		this.bookPrice = bookPrice;
+	}
+	@Override
+	public String toString() {
+		return "Book\nBookName=" + bookName + ",\nBookAuthor=" + bookAuthor + ",\nBookPrice=" + bookPrice + "\n";
+	}
+}
